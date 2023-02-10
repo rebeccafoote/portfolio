92b5bf5bda6bea436e7e1d5f5110514be0b6d9ff
rebeccafoote <59989821+rebeccafoote@users.noreply.github.com>
2023-02-09 22:55:08 -0500

    Add files via upload

---
+++ not_in_use/navigation.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<header>
+    <h1><a href="splash.html">Rebecca Foote</a></h1>
+    <div class="nav_menu">
+        <nav>
+        <ul class="ul">
+            <li><a href="about.html">About</a></li>
+            <li class="dropdown">
+                <a href="thesis.html">Thesis</a>
+                <ul class="drop_content">
+                    <a href="t-collection.html">Main Collection</a>
+                    <a href="t-color_study.html">Color Study</a>
+                </ul>
+            <li class="dropdown">
+                <a href="past_work.html">Past Work</a>
+                <ul class="drop_content">
+                    <a href="2-origami_bags.html">Origami Bags</a>
+                    <a href="3-in_out.html">Inward / Outward</a>
+                    <a href="4-box_shorts.html">Box Shorts</a>
+                    <a href="5-float.html">Float</a>
+                    <a href="6-bleach.html">Bleach</a>
+                </ul>
+        </ul>
+    </nav>
+    </div>
+</header>
